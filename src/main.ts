ffd78aeddaf54c324e14a44c272778e85b7cbcc2
유동수 <badongsch3@gmail.com>
2024-06-26 15:50:58 +0900

    수정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 8080; // Ensure port is set to 8080
+  const port = configService.get<number>('PORT') || 80; // Ensure port is set to 8080
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
