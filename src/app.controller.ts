49f1bb59093115b49e8ede63ee8643a107583e09
유동수 <badongsch3@gmail.com>
2024-07-05 11:08:36 +0900

    수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  getUptime() {
+    return this.appService.getUptime();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World2!';
   }
+
+  getUptime() {
+    const now = new Date();
+    const formattedNow = now
+      .toISOString()
+      .replace(/T/, ' ')
+      .replace(/\..+/, '');
+    return `<b>Server is running for</b> ${process.uptime()} seconds. <br><b>Nowtime(UTC)</b> ${formattedNow} <br>`;
+  }
 }
