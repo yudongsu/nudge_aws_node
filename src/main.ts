b510e808ae410948c0c8ae3e75f15b9595307ae8
유동수 <badongsch3@gmail.com>
2024-07-08 11:39:01 +0900

    수정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 // .env 파일을 로드
 dotenv.config();
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
